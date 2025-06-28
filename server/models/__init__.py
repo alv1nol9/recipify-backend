52aec17636f8ab3acf4cc3d4b376ef049ffd69c7
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-28 22:32:54 +0300

    final fix

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,8 +1,3 @@
-from .base import db
 from .user import User
 from .recipe import Recipe
 from .comment import Comment
-
-# Fully-qualified string paths to avoid resolution issues
-User.recipes = db.relationship('Recipe', backref='author', lazy=True)
-User.comments = db.relationship('Comment', backref='author', lazy=True)
