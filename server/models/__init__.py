488c3b746511bcb0cdce45d818081709d5df6585
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-28 22:24:45 +0300

    fix: use local model names in User relationships

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -4,5 +4,5 @@ from .recipe import Recipe
 from .comment import Comment
 
 # Fully-qualified string paths to avoid resolution issues
-User.recipes = db.relationship('server.models.recipe.Recipe', backref='author', lazy=True)
-User.comments = db.relationship('server.models.comment.Comment', backref='author', lazy=True)
+User.recipes = db.relationship('Recipe', backref='author', lazy=True)
+User.comments = db.relationship('Comment', backref='author', lazy=True)
