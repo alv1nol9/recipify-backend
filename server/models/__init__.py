e29daf453cee2cb7ff569bda5f0b51771cd1a055
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-28 10:11:35 +0300

    Fix model import order to resolve relationship error

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from .user import User
 from .recipe import Recipe
 from .comment import Comment
+from .user import User
 
 __all__ = ['db', 'User', 'Recipe', 'Comment']
