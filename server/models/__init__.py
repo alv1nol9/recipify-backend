7aa0ca4cb57ab87e8a963f29671c75e88a2abf9d
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-06-28 21:53:41 +0300

    🔥 fix: import db in __init__.py

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,7 +1,7 @@
+from .base import db
 from .user import User
 from .recipe import Recipe
 from .comment import Comment
 
-# Delayed relationship binding to avoid circular imports
 User.recipes = db.relationship('Recipe', backref='author', lazy=True)
 User.comments = db.relationship('Comment', backref='author', lazy=True)
